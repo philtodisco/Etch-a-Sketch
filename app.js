2f54ef030124637ff4b842aa85eb117ce13f624a
Phil Todisco <philtodisco@phils-mbp.lan>
2021-12-08 21:39:16 -0500

    Create grid using gridSize variable

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const gridContainer = document.querySelector('#grid-container')
+const gridContainer = document.querySelector('.grid-container')
 let gridSize = 256
 
 for(i = 0; i < gridSize; i++) {
     const grid = document.createElement('div');
-    grid.textContent = '.';
+    grid.classList.add('grid')
+    grid.textContent = ' ';
     gridContainer.appendChild(grid);
 }
 
