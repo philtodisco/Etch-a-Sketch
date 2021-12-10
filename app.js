358b57ba861b290579bb32f233d78f649898e41f
Phil Todisco <philtodisco@phils-mbp.lan>
2021-12-09 23:02:52 -0500

    Change square color using mouseover

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const gridContainer = document.querySelector('.grid-container')
-let gridSize = 256
+const gridItem = document.getElementsByClassName('.grid')
+let gridSize = 16
+let grideSize2 = Math.pow(gridSize, 2)
 
-for(i = 0; i < gridSize; i++) {
+for(i = 0; i < grideSize2; i++) {
     const grid = document.createElement('div');
     grid.classList.add('grid')
     grid.textContent = ' ';
     gridContainer.appendChild(grid);
+    grid.addEventListener('mouseover', () => {
+        grid.style.backgroundColor = 'gray'
+    })
 }
+
+// gridItem.forEach((grid) => {
+//     grid.addEventListener('mouseover', () => {
+//         alert('ok')
+//     })
+// })
+
+
 
 
 
