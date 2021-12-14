5f74ae92fb6fb707fb78f4321e1b161685810ea3
Phil Todisco <philtodisco@phils-mbp.lan>
2021-12-14 17:20:23 -0500

    Responive grid size using inline css and template literal

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const gridContainer = document.querySelector('.grid-container')
 const gridItem = document.getElementsByClassName('.grid')
-let gridSize = 16
-let grideSize2 = Math.pow(gridSize, 2)
+let squaresPerSide = 16
+let grideSize2 = Math.pow(squaresPerSide, 2)
 
+// add blocks using for loop
 for(i = 0; i < grideSize2; i++) {
+    //
     const grid = document.createElement('div');
     grid.classList.add('grid')
     grid.textContent = ' ';
+    gridContainer.style = `grid-template-columns: repeat(${squaresPerSide}, 25px [col-start]);`
     gridContainer.appendChild(grid);
     grid.addEventListener('mouseover', () => {
         grid.style.backgroundColor = 'gray'
-    })
+    }) 
 }
 
-// gridItem.forEach((grid) => {
-//     grid.addEventListener('mouseover', () => {
-//         alert('ok')
-//     })
-// })
+
+
+
+
 
 
 
