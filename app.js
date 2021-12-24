68b6e63ea539a2a18080d40fe3ea6104605d3a6b
Phil Todisco <philtodisco@phils-mbp.lan>
2021-12-23 20:24:38 -0500

    Remove color picker

--- app.js
+++ app.js
@@ -1,51 +1,24 @@
 const gridContainer = document.querySelector('.grid-container')
-const gridItem = document.getElementsByClassName('.grid')
-let squaresPerSide = 16
-let grideSize2 = Math.pow(squaresPerSide, 2)
+const block = document.getElementsByClassName('.block')
+let blocksPerSide = 16
+let gridSizeSquared = Math.pow(blocksPerSide, 2)
 
 // add blocks using for loop
-for(i = 0; i < grideSize2; i++) {
-    //
-    const grid = document.createElement('div');
-    grid.classList.add('grid')
-    grid.textContent = ' ';
-    gridContainer.style = `grid-template-columns: repeat(${squaresPerSide}, 25px [col-start]);`
-    gridContainer.appendChild(grid);
-    grid.addEventListener('mouseover', () => {
-        grid.style.backgroundColor = 'gray'
-    }) 
+createGrid = () => {
+    for(i = 0; i < gridSizeSquared; i++) {
+        const block = document.createElement('div');
+        block.classList.add('block')
+        block.textContent = ' ';
+        gridContainer.style = `grid-template-columns: repeat(${blocksPerSide}, 25px [col-start]);`
+        gridContainer.appendChild(block);
+        block.addEventListener('mouseover', () => {
+            block.style.backgroundColor = 'gray'
+        }) 
+    }
 }
 
-let colorWell;
-let defaultColor = "#0000ff";
 
-window.addEventListener("load", startup, false);
-
-function startup() {
-    colorWell = document.querySelector("#colorWell");
-    colorWell.value = defaultColor;
-    colorWell.addEventListener("input", updateFirst, false);
-    colorWell.addEventListener("change", updateAll, false);
-    colorWell.select();
-  }
-  
-  function updateFirst(event) {
-    let grid = document.querySelector(".grid");
-  
-    if (grid) {
-      grid.style.backgroundColor = event.target.value;
-    }
-  }
-  
-  function updateAll(event) {
-    document.querySelectorAll(".grid").forEach(function(grid) {
-      grid.style.backgroundColor = event.target.value;
-    });
-  }
-  
-
-
-
+createGrid()
 
 
 
