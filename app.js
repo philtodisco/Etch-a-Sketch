c2dbab0342e947756ad0f7478c8dd1a71cc8372a
Phil Todisco <philtodisco@phils-mbp.lan>
2021-12-28 17:03:30 -0500

    Add change-size button

--- app.js
+++ app.js
@@ -1,40 +1,19 @@
-// const gridContainer = document.querySelector('.grid-container')
-// const block = document.getElementsByClassName('.block')
-// let blocksPerSide = 16
-// let gridSizeSquared = Math.pow(blocksPerSide, 2)
-
-// // add blocks using for loop
-// createGrid = () => {
-//     for(i = 0; i < gridSizeSquared; i++) {
-//         const block = document.createElement('div');
-//         block.classList.add('block')
-//         block.textContent = ' ';
-//         gridContainer.style = `grid-template-columns: repeat(${blocksPerSide}, 25px [col-start]);`
-//         gridContainer.appendChild(block);
-//         block.addEventListener('mouseover', () => {
-//             block.style.backgroundColor = 'gray'
-//         }) 
-//     }
-// }
-
-
-// createGrid()
-
 const gridContainer = document.querySelector('.grid-container')
 const block = document.getElementsByClassName('.block')
-let size = 50
-// let gridSizeSquared = Math.pow(blocksPerSide, 2)
+let defaultSize = 50
+let changeSize = prompt
 
-// add blocks using for loop
-createGrid = (size) => {
-    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+// add blocks to create a grid using for loop
+createGrid = (defaultSize) => {
+    gridContainer.style.gridTemplateColumns = `repeat(${defaultSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${defaultSize}, 1fr)`;
     
-    for(i = 0; i < size * size; i++) {
+    for(i = 0; i < defaultSize * defaultSize; i++) {
         const block = document.createElement('div')
         block.classList.add('block')
         gridContainer.appendChild(block)
     }
+    // change color of a block on "mouseover"
     blocks = document.querySelectorAll(".block")
     blocks.forEach((block) => {
         block.addEventListener("mouseover", (e) => {
@@ -43,7 +22,11 @@ createGrid = (size) => {
     })
 }
 
-createGrid(size)
+changeSize = ()
+
+
+
+createGrid(defaultSize)
 
 
 
