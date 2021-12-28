72827ef907156f6a291088986e680c38fb66b0f4
Phil Todisco <philtodisco@phils-mbp.lan>
2021-12-28 17:30:59 -0500

    Add changeSize function

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const gridContainer = document.querySelector('.grid-container')
 const block = document.getElementsByClassName('.block')
-let defaultSize = 50
-let changeSize = prompt
+const changeSize = document.getElementById('change-size')
+let size = 50
+
 
 // add blocks to create a grid using for loop
-createGrid = (defaultSize) => {
-    gridContainer.style.gridTemplateColumns = `repeat(${defaultSize}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${defaultSize}, 1fr)`;
+createGrid = (size) => {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
-    for(i = 0; i < defaultSize * defaultSize; i++) {
+    for(i = 0; i < size * size; i++) {
         const block = document.createElement('div')
         block.classList.add('block')
         gridContainer.appendChild(block)
@@ -22,11 +23,21 @@ createGrid = (defaultSize) => {
     })
 }
 
-changeSize = ()
+changeSize.addEventListener("click", (e) => {
+    blocks.forEach((block) => {
+        block.classList.remove("mouseover")
+    })
+    size = prompt("Enter a number between 16-100")
+    // prevent user from 
+    if (size < 16 || size > 100 || isNaN(size)) {
+        size = prompt("Enter a number between 16-100")
+    }
+    createGrid(size)
+})
 
 
 
-createGrid(defaultSize)
+createGrid(size)
 
 
 
